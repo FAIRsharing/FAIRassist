39ece89f2bd49d98c6a40fa06de9c8b71a3399d9
Terazus <batistadominique@hotmail.com>
2019-06-05 11:08:20 +0100

    styling + refactoring controller

--- app/home/unknownCtrl.js
+++ app/home/unknownCtrl.js
@@ -1,5 +1,14 @@
 define(['app'], function (app) {
     app.register.controller('View1Controller', ['$scope', function ($scope) {
-        $scope.title = "Page Title 1";
+
+        $scope.data = {}
+        $scope.data.title = "Page title unknown yet";
+        $scope.data.labels = [
+            "label 1",
+            "label 2",
+            "label 3"
+        ]
+
+
     }])
 });
