5bf0b6e75b404e0c3c0884b13481c0345764a25b
Terazus <batistadominique@hotmail.com>
2019-06-07 11:39:09 +0100

    styling table

--- app/home/unknownCtrl.js
+++ app/home/unknownCtrl.js
@@ -1,13 +1,18 @@
 define(['app'], function (app) {
     app.register.controller('View1Controller', ['$scope', function ($scope) {
 
-        $scope.data = {}
-        $scope.data.title = "Page title unknown yet";
+        $scope.data = {};
+        $scope.data.title = "List of FAIR evaluation services";
         $scope.data.labels = [
             "label 1",
             "label 2",
-            "label 3"
+            "label 3",
+            "label 4",
+            "label 5"
+        ];
+        $scope.data.content = [
         ]
+
 
 
     }])
