7b92f46336672feeae9552f706417818e41f820e
Terazus <batistadominique@hotmail.com>
2019-06-07 12:34:14 -0700

    auto-responsivness: switch from table to grid on sm breakpoint

--- app/home/unknownCtrl.js
+++ app/home/unknownCtrl.js
@@ -1,6 +1,7 @@
 define(['app'], function (app) {
-    app.register.controller('View1Controller', ['$scope', function ($scope) {
+    app.register.controller('View1Controller', ['$scope', '$window', function ($scope, $window) {
 
+        /* CONTENT */
         $scope.data = {};
         $scope.data.title = "List of FAIR evaluation services";
         $scope.data.labels = [
@@ -11,7 +12,39 @@ define(['app'], function (app) {
             "label 5"
         ];
         $scope.data.content = [
-        ]
+            {
+                "abc": "defdefde fdefd efdefdefdefdefd ef",
+                "def": "def defdef fdefdef def",
+                "ijk": "def",
+                "lmn": "def",
+                "opc": "def"
+            },
+            {
+                "abc": "def",
+                "def": "def",
+                "ijk": "def",
+                "lmn": "def",
+                "opc": "def"
+            },
+            {
+                "abc": "def",
+                "def": "def",
+                "ijk": "def",
+                "lmn": "def",
+                "opc": "def"
+            }
+        ];
+
+        /* DISPLAY */
+        $scope.display = {};
+        $scope.display.mobile = ($window.innerWidth <= 556) ;
+
+        angular.element($window).bind('resize', function(){
+           $scope.display.mobile = ($window.innerWidth <= 556) ;
+           $scope.$digest();
+           console.log($scope.display.mobile);
+        });
+
 
 
 
