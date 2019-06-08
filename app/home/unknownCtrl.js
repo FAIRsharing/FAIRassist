5877ad55eef9c21257a04e42fba3152ff3b58583
Terazus <batistadominique@hotmail.com>
2019-06-08 05:36:21 -0700

    added autoparsing data from google spreadsheet json api

--- app/home/unknownCtrl.js
+++ app/home/unknownCtrl.js
@@ -4,36 +4,41 @@ define(['app'], function (app) {
         /* CONTENT */
         $scope.data = {};
         $scope.data.title = "List of FAIR evaluation services";
-        $scope.data.labels = [
-            "label 1",
-            "label 2",
-            "label 3",
-            "label 4",
-            "label 5"
-        ];
-        $scope.data.content = [
-        ];
-
-        /* DISPLAY */
+        $scope.data.content = [];
         $scope.display = {};
-        $scope.display.mobile = ($window.innerWidth <= 556) ;
+        $scope.display.mobile = ($window.innerWidth <= 900) ;
 
+        /* trigger the digest cycle when the window in re-sized to compute width vies */
         angular.element($window).bind('resize', function(){
-           $scope.display.mobile = ($window.innerWidth <= 556) ;
+           $scope.display.mobile = ($window.innerWidth <= 900) ;
            $scope.$digest();
-           console.log($scope.display.mobile);
         });
 
+        /* Accessing the spreadsheet */
         let spreadsheetID = "1gzt5eYLk-5MJ-EC1s8KDq5fpFcNQxa47PUWAGk2gHzw";
         let request = {
             method: 'GET',
             url: "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/1/public/full?alt=json"
         };
-
         $http(request).then(function(response){
-            console.log(response)
-            $scope.data.content = response.data['feed']['entry']
-        })
+            $scope.data.content = response.data['feed']['entry'];
+            $scope.display.labels = build_labels(response.data['feed']['entry'][0]);
+        });
+
+        /* PRIVATE FUNCTIONS */
+
+        let build_labels = function(item){
+            /* Builds the labels using the first item in the response list */
+            let labels = [];
+            for (let field in item){
+                if (item.hasOwnProperty(field) && field.indexOf('gsx$') !== -1){
+                    let label = field.replace('gsx$', '');
+                    labels.push({property: field, label: label})
+                }
+            }
+
+            return labels;
+        }
 
 
 
